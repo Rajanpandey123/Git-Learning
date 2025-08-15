2a8875a3130a407047b56162f1f9116ff98db7f4
Rajan pandey <rajan18nove@gmail.com>
2025-08-15 20:45:22 +0530

    upi added

--- first.js
+++ first.js
@@ -4,5 +4,7 @@ console.log(button);
 const login="Login page added";
 console.log(login);
 
-const payment="Integrated payment system"
+const payment="Integrated payment system";
 console.log(payment);
+const upi="upi added";
+console.log(upi);
